a52abd8cbcdb27c4516f4aabbe472d1aa823463f
Hamza Arab <hamza.arab@live.com>
2022-01-23 23:25:20 +0500

    About Page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,13 +4,20 @@
 @section('content')
 <section>
     <div class="container">
+        <h1 class="mb-4">About Us</h1>
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem officiis ab nesciunt quo! Ex amet illum, soluta nisi perspiciatis ratione quam maxime velit fugiat harum incidunt non ipsa facilis?
-        </p>
-        <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nulla adipisci veniam a laboriosam doloribus ipsum officia eligendi dolorum necessitatibus amet, similique eius voluptas quae cum ratione quia cupiditate error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat, saepe placeat praesentium cumque vitae aut doloremque, mollitia libero unde consectetur, excepturi commodi quas fugit. Ipsa atque quaerat dignissimos reprehenderit!
         </p>
         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem officiis ab nesciunt quo! Ex amet illum, soluta nisi perspiciatis ratione quam maxime velit fugiat harum incidunt non ipsa facilis?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nulla adipisci veniam a laboriosam doloribus ipsum officia eligendi dolorum necessitatibus amet, similique eius voluptas quae cum ratione quia cupiditate error.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat, saepe placeat praesentium cumque vitae aut doloremque, mollitia libero unde consectetur, excepturi commodi quas fugit. Ipsa atque quaerat dignissimos reprehenderit!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam exercitationem officiis ab nesciunt quo! Ex amet illum, soluta nisi perspiciatis ratione quam maxime velit fugiat harum incidunt non ipsa facilis?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nulla adipisci veniam a laboriosam doloribus ipsum officia eligendi dolorum necessitatibus amet, similique eius voluptas quae cum ratione quia cupiditate error.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat, saepe placeat praesentium cumque vitae aut doloremque, mollitia libero unde consectetur, excepturi commodi quas fugit. Ipsa atque quaerat dignissimos reprehenderit!
         </p>
     </div>
