bda32beb643a1eccf138f17f05dc70b1bf4a3d7a
muneebkh2 <muneebkh5@gmail.com>
2022-01-26 12:47:56 +0500

    seeders updated.

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -19,94 +19,75 @@ class CategoriesTableSeeder extends Seeder
         $categories = [
             [
                 'name' => 'Electrical Switches',
-                // 'slug' => 'automotive',
             ],
             [
                 'name' => 'Commercial Lights',
-                // 'slug' => 'business-services',
             ],
             [
                 'name' => 'Emergency Lights',
-                // 'slug' => 'computer-telecom-&-it-services',
             ],
             [
                 'name' => 'Sensors',
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Fliton',
                 'parent_id' => 1
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Brock',
                 'parent_id' => 1
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Balfon',
                 'parent_id' => 1
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Euro Module',
                 'parent_id' => 1
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Robust',
                 'parent_id' => 1
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Indoor Lighting',
                 'parent_id' => 2
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Outdoor Lighting',
                 'parent_id' => 2
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'LED Exit Signs',
                 'parent_id' => 3
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'LED Exit Lights',
                 'parent_id' => 3
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Emergency Drivers',
                 'parent_id' => 3
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Brushed Steel',
                 'parent_id' => 5
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Polished Gold',
                 'parent_id' => 5
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Polished Nickel',
                 'parent_id' => 5
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Weatherproof',
                 'parent_id' => 9
-                // 'slug' => 'education-&-training',
             ],
             [
                 'name' => 'Metal Clad',
                 'parent_id' => 9
-                // 'slug' => 'education-&-training',
             ],
         ];
 
