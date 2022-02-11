2db2d52eb8273407a1c245eae2be3cf695b7cc23
muneebkh2 <muneebkh5@gmail.com>
2022-02-12 02:57:00 +0500

    categories url and display completed.

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -15,6 +15,7 @@
     <section id="categories-grid">
 
         <div class="row no-gutters category-row" style="background: url('./assets/category/bg1.png') no-repeat center / cover;">
+            {{-- @dd($categories) --}}
             @foreach ($categories as $category)
 
             {{-- @dd($category->subcategory) --}}
@@ -24,7 +25,7 @@
                     <div class="col-md-6 category-col">
                         <div class="category">
                             <a href="#">
-                                <h5 class="category-title"><a href="category/{{$category->slug}}/{{$subcategory->slug}}">{{$subcategory->name}}</a></h5>
+                                <h5 class="category-title"><a href="{{Request::url()}}/{{$subcategory->slug}}">{{$subcategory->name}}</a></h5>
                                 <img src="./assets/category/Brushed-steel-fliton.png" alt="Brushed-steel-fliton" class="img-fluid" />
                             </a>
                         </div>
