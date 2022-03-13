f950a2529ed629c0fd0fd822f5a5ccc8e92d7446
muneeb <muneeb.rehman@rltsquare.com>
2022-03-14 01:48:08 +0500

    data table and style added.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,44 +6,63 @@
                 <a href="{{ route('home') }}"><img src="{{ asset('assets/Hapton-Logo.png') }}" alt="Hapton" class="img-fluid"></a>
             </div>
 
-            <div class="navigation-toggle">
-                <a href="javascript:void(0)" class="he-menu-opener">
-                    <span class="he-menu-close-icon">
-                        <span class="he-hm-lines">
-                        <span class="he-hm-line he-line-1"></span>
-                        <span class="he-hm-line he-line-2"></span>
-                        </span>
-                    </span>
-                    <span class="he-menu-opener-icon">
-                        <span class="he-hm-lines">
+            @if(Auth::check())
+                <nav class="admin-navigation">
+                    <div class="nav-container">
+                        <ul class="menu">
+                            <li class="menu-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="menu-item"><a href="{{ route('category') }}">Category</a></li>
+                            <li class="menu-item">
+                                <a href="{{ url('/admin/products') }}"> Products List </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="{{ url('/admin/products/create') }}"> Add New Product </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="{{ url('/logout') }}" class="text-danger"> Logout </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            @else
+                <div class="navigation-toggle">
+                    <a href="javascript:void(0)" class="he-menu-opener">
+                        <span class="he-menu-close-icon">
+                            <span class="he-hm-lines">
                             <span class="he-hm-line he-line-1"></span>
                             <span class="he-hm-line he-line-2"></span>
-                            <span class="he-hm-line he-line-3"></span>
+                            </span>
                         </span>
-                    </span>
-                </a>
-            </div>
-
-            <nav id="site-navigation">
-                <div class="nav-container">
-                    <ul class="menu">
-                        <li class="menu-item"><a href="{{ route('home') }}">Home</a></li>
-                        <li class="menu-item"><a href="{{ route('category') }}">Categories</a></li>
-                        @if (Auth::check())
-                        <li class="menu-item">
-                            <a href="{{ url('/admin/products/create') }}"> Add New Product </a>
-                        </li>
-                        <li class="menu-item">
-                            <a href="{{ url('/logout') }}"> Logout </a>
-                        </li>
-                        @else
-                        <li class="menu-item">
-                            <a href="{{ url('/login') }}"> Login </a>
-                        </li>
-                        @endif
-                    </ul>
+                        <span class="he-menu-opener-icon">
+                            <span class="he-hm-lines">
+                                <span class="he-hm-line he-line-1"></span>
+                                <span class="he-hm-line he-line-2"></span>
+                                <span class="he-hm-line he-line-3"></span>
+                            </span>
+                        </span>
+                    </a>
                 </div>
-            </nav>
+                <nav id="site-navigation">
+                    <div class="nav-container">
+                        <ul class="menu">
+                            <li class="menu-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="menu-item"><a href="{{ route('category') }}">Categories</a></li>
+                            {{--@if (Auth::check())
+                            <li class="menu-item">
+                                <a href="{{ url('/admin/products/create') }}"> Add New Product </a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="{{ url('/logout') }}"> Logout </a>
+                            </li>
+                            @else
+                            <li class="menu-item">
+                                <a href="{{ url('/login') }}"> Login </a>
+                            </li>
+                            @endif--}}
+                        </ul>
+                    </div>
+                </nav>
+            @endif
         </div>
     </div>
 </header>
