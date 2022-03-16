41fb1b3f86dc111ce2b47bcd776a6d1ca7d25e2a
muneebkh2 <muneebkh5@gmail.com>
2022-03-16 06:07:59 +0500

    favicon added.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <link rel="icon" href="{{ asset('assets/logo/hapton-favicon.png') }}"/>
+
         <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
 
         <link rel="stylesheet" href="{{ asset('/css/owl.carousel.min.css') }}">
