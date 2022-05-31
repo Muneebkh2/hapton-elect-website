66e2f10b7512c073c03fb59d793fed175f5c8b6f
unknown <arsalanmughal23@gmail.com>
2022-06-01 02:18:07 +0500

    ui-fix

--- resources/views/pages/products/detail.blade.php
+++ resources/views/pages/products/detail.blade.php
@@ -32,46 +32,13 @@
                 <h1 class="product-title mb-md-3">{{$product->name}}</h1>
 
                 <div class="cs-responsive product-info">
-                    <!-- <ul class="specification-list">
+                    <ul class="row specification-list">
                         @forelse ($product->attributes as $attribute)
-                        <li><span class="point-heading">{{$attribute->name}} : </span> <p>{{$attribute->value}}</p></li>
+                            <div class="col-4"><span class="point-heading">{{$attribute->name}}: </span></div>
+                            <div class="col-8">{{$attribute->value}}</div>
                         @empty
                         @endforelse
-                    </ul> -->
-                    
-                    <div class="row specification-list" style="height: 100px;overflow: auto;">
-                        @forelse ($product->attributes as $attribute)
-                        <div class="col col-md-auto py-2 border-top">
-                            <div class="point-heading">{{$attribute->name}} </div> 
-                            <span>{{$attribute->value}}</span>
-                        </div>
-                        @empty
-                        @endforelse
-                    </div>
-
-                    <!-- <div class="d-flex specification-list" style="overflow-x: scroll;">
-                        @forelse ($product->attributes as $attribute)
-                        <div class="py-2 px-2 border">
-                            <div style="inline-size: max-content;">
-                                <div class="point-heading">{{$attribute->name}} </div> 
-                                <span>{{$attribute->value}}</span>
-                            </div>
-                        </div>
-                        @empty
-                        @endforelse
-                    </div> -->
-
-                    <!-- <table class="table table-bordered specification-list">
-                        <tbody>
-                            @forelse ($product->attributes as $attribute)
-                            <tr>
-                                <th scope="col" class="point-heading">{{$attribute->name}} </th> 
-                                <td>{{$attribute->value}}</td>
-                            </tr>
-                            @empty
-                            @endforelse
-                        </tbody>
-                    </table> -->
+                    </ul>
 
                     @php
                         $productHeader = collect($product->dynamic_table_header);
@@ -79,60 +46,28 @@
                     @endphp
 
                     @if(count($productHeader) > 0 && $productHeader->get('1') != null)
-                    <h4 class="mb-2 mt-5">Chart</h4>
-                    <style>
-                        /* .h-50px{height: 40px}; */
-                    </style>
-                    <!-- <div class="row">
-                        <div class="col-2">
-                            @foreach ($productHeader as $headerKey => $valueKey)
-                                <div class="h5 mb-0 h-50px d-flex flex-column-reverse">{{$valueKey}}</div>
-                            @endforeach
-                        </div>
-                        <div class="col-9 d-flex offset-md-0 offset-sm-1" style="overflow:auto;">
-                            @foreach ($productBody as $productBodyItem)
-                                <div class="tbl-inner-row">
-                                    @foreach ($productBodyItem as $productBodyRow)
-                                        <div class="h-50px border-bottom px-2 d-flex flex-column-reverse">{{ $productBodyRow }}</div>
-                                    @endforeach
-                                </div>
-                            @endforeach
-                        </div>
-                    </div> -->
+                    <h4 class="mb-2 mt-5">Models: </h4>
                     
-                    <table class="table table-dark table-responsive table-striped">
-                        @foreach ($productHeader as $headerKey => $valueKey)
-                            <tr class="d-flex align-items-center">
-                                <th class="" style="width: 120px;word-break: break-word;">{{$valueKey}}</th>
-                                @foreach ($productBody[$headerKey] as $productBodyRow)
-                                    <td class="pl-2">{{ $productBodyRow }}</td>
-                                @endforeach
-                            </tr>
-                        @endforeach
-                    </table>
-                    
-                    <!-- <div class="table-responsive">
+                    <div class="table-responsive">
                         <table class="table text-center">
                             <thead>
                                 <tr>
                                     @foreach ($productHeader as $headerKey => $valueKey)
-                                    <td @if ($headerKey == 2)
-                                        width="35%" @elseif($headerKey == 5) width="20%"
-                                    @endif>{{$valueKey}}</td>
+                                    <td class="pr-2">{{$valueKey}}</td>
                                     @endforeach
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($productBody as $productBodyItem)
-                                <tr>
+                                <tr class="border-bottom">
                                     @foreach ($productBodyItem as $productBodyRow)
-                                       <td>{{ $productBodyRow }}</td>
+                                       <td class="pr-2">{{ $productBodyRow }}</td>
                                     @endforeach
                                 </tr>
                                 @endforeach
                             </tbody>
                         </table>
-                    </div> -->
+                    </div>
                     @endif
 
                     {{-- <h4>Models</h4>
